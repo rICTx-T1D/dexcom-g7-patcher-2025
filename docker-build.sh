7b2fdee0ff42c1e7be9830faecec6f101901138c
DreadPirateRoberts <46958197+ManfredRichthofen@users.noreply.github.com>
2024-11-07 08:39:41 -0800

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,4 +2,4 @@
 
 rm -rf dexcom.patched.apk
 docker build --no-cache -t diakem-dexcom-g7-apk-patcher .
-docker run -it -v $(pwd):/output diakem-dexcom-g7-apk-patcher 
+docker run -v $(pwd):/output diakem-dexcom-g7-apk-patcher 
